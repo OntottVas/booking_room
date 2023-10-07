e5a0ea840c9dc7b66226e31423f656c52245f4cd
Geree <smithzero94@gmail.com>
2023-10-07 16:12:58 +0200

    RoomService class, modify room for bookings implemented.

--- src/main/java/hu/progmatic/booking_room/controller/BookingController.java
+++ src/main/java/hu/progmatic/booking_room/controller/BookingController.java
@@ -1,22 +1,21 @@
 package hu.progmatic.booking_room.controller;
 
-import hu.progmatic.booking_room.model.Booking;
 import hu.progmatic.booking_room.model.Guest;
+import hu.progmatic.booking_room.model.Room;
 import hu.progmatic.booking_room.service.BookingService;
 import hu.progmatic.booking_room.service.GuestService;
+import hu.progmatic.booking_room.service.RoomService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.Optional;
 
 @Controller
 @AllArgsConstructor
 public class BookingController {
     private BookingService bookingService;
     private GuestService guestService;
+    private RoomService roomService;
 
     @GetMapping("/bookings")
     public String getAllBookings(Model model) {
@@ -28,6 +27,12 @@ public class BookingController {
     public String getAllGuests(Model model) {
         model.addAttribute("guests", guestService.getAllGuests());
         return "guests";
+    }
+
+    @GetMapping("/rooms")
+    public String getAllRooms(Model model) {
+        model.addAttribute("rooms", roomService.getAllRooms());
+        return "rooms";
     }
 
     @GetMapping("/addGuest")
@@ -49,8 +54,20 @@ public class BookingController {
 
     @PostMapping("/modifyGuest")
     public String modifyGuest(@RequestParam("id") Long id, @RequestParam("guest_id") Long guest_id) {
-        Guest guest = guestService.getGuestById(guest_id);
-        bookingService.modifyGuest(id, guest);
+        Guest otherGuest = guestService.getGuestById(guest_id);
+        bookingService.modifyGuest(id, otherGuest);
+        return "redirect:/bookings";
+    }
+
+    @GetMapping("/modifyRoom")
+    public String modifyRoom() {
+        return "modifyRoom";
+    }
+
+    @PostMapping("/modifyRoom")
+    public String modifyRoom(@RequestParam("id") Long id, @RequestParam("room_id") Long room_id) {
+        Room otherRoom = roomService.getRoomById(room_id);
+        bookingService.modifyRoom(id, otherRoom);
         return "redirect:/bookings";
     }
 }
--- src/main/java/hu/progmatic/booking_room/service/RoomService.java
+++ src/main/java/hu/progmatic/booking_room/service/RoomService.java
@@ -1,13 +1,45 @@
 package hu.progmatic.booking_room.service;
 
+import hu.progmatic.booking_room.model.Room;
 import hu.progmatic.booking_room.repository.RoomRepository;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class RoomService {
     private RoomRepository roomRepository;
+    public List<Room> getAllRooms() {
+        return roomRepository.findAll();
+    }
 
+    public Room getRoomById(Long id) {
+        return roomRepository.findById(id).orElse(null);
+    }
+
+    public void modifyCapacity(Long id, Integer newCapacity) {
+        Room room = getRoomById(id);
+        room.setCapacity(newCapacity);
+        roomRepository.save(room);
+    }
+
+    public void modifyPrice(Long id, Integer newPrice) {
+        Room room = getRoomById(id);
+        room.setPrice(newPrice);
+        roomRepository.save(room);
+    }
+
+    public void modifyJacuzzi(Long id, Boolean hasJacuzzi) {
+        Room room = getRoomById(id);
+        room.setJacuzzi(hasJacuzzi);
+        roomRepository.save(room);
+    }
+
+    public void modifySauna(Long id, Boolean hasSauna) {
+        Room room = getRoomById(id);
+        room.setSauna(hasSauna);
+        roomRepository.save(room);
+    }
 }
